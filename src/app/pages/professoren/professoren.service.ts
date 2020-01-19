100c24408d46dab1ce57a99de37f6ebccf7fce58
bugyman <torben_barmstedt@musicmeister.de>
2020-01-19 19:06:17 +0100

    2 files modified

--- src/app/pages/professoren/professoren.service.ts
+++ src/app/pages/professoren/professoren.service.ts
@@ -7,7 +7,7 @@ export class ProfessorenService {
   professoren=[
     new Prof("https://hs-flensburg.de/sites/default/files/styles/portraitfoto/public/2019-05/Christoph_Jansen_ausschnitt.jpg.jpeg?itok=4-ImiMP8","Christoph","Jansen","Präsidium:Präsident","Raum H32"),
     new Prof("https://hs-flensburg.de/sites/default/files/styles/portraitfoto/public/2017-10/looks_volker.jpg.jpeg?itok=-sBtOfvb","Volker","Looks","Professor am Fachbereich Wirtschaft","Dekan",),
-    new Prof("https://hs-flensburg.de/sites/default/files/styles/portraitfoto/public/2017-11/petersen_kai.jpg.jpeg?itok=JVf1P7aR","Kai","Petersen","Professor am Fachbereich Wirtschaft","Senat:Professor",),
+    new Prof("https://hs-flensburg.de/sites/default/files/styles/portraitfoto/public/2017-11/petersen_kai.jpg.jpeg?itok=JVf1P7aR","Kai","Petersen","Professor am Fachbereich Wirtschaft","Senat:Professor"),
    
   ]
   post={
@@ -16,13 +16,13 @@ export class ProfessorenService {
     name: "", 
     bereich: "",
     rang:"",
-    mail:""
+ 
   }
 
   constructor() { }
    onAddworker(){
      this.professoren.push(
-       new Prof(this.post.img,this.post.vorname,this.post.name,this.post.bereich,this.post.mail,this.post.rang)
+       new Prof(this.post.img,this.post.vorname,this.post.name,this.post.bereich,this.post.rang)
      )
    };
    onDelete(index:number){
