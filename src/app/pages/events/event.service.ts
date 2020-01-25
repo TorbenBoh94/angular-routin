c6155b77d60fbf4e19d35b5fd3c8a093ed9c2538
bugyman <torben_barmstedt@musicmeister.de>
2020-01-25 15:45:13 +0100

    2 files modified

--- src/app/pages/events/event.service.ts
+++ src/app/pages/events/event.service.ts
@@ -5,8 +5,9 @@ import{Events}from './events'
 export class EventService {
  
  edit=false;
- 
- events=[ new Events("Hochschulfest","Gemütliches Beisammensitzen der Studenten und Professoren","https://me2be.de/wp-content/uploads/2018/06/CAMPUS_Sommerfest_HSFL_Dorbandt_1.jpg","2020-08-15","15.00"),
+ index:number;
+ events=[ new Events("Hochschulfest","Gemütliches Beisammensitzen der Studenten und Professoren","https://me2be.de/wp-content/uploads/2018/06/CAMPUS_Sommerfest_HSFL_Dorbandt_1.jpg","2020-08-15","15:00"),
+ new Events("Semesterbeginn","Das Semester beginnt und das Lernen kann wieder anfangen","https://hs-flensburg.de/sites/default/files/styles/full_width/public/mood/termine_Web.jpg?itok=w1xB9zKc","2020-03-01",""),
  ];
 
  post={
@@ -37,5 +38,15 @@ export class EventService {
   }
   onDelete(index:number){
     this.events.splice(index,1)
+  }
+  onEdit(index:number){
+    this.edit=true;
+      
+    this.post.name=this.events[index].name;
+    this.post.beschreibung=this.events[index].beschreibung;
+    this.post.img=this.events[index].img;
+    this.post.date=this.events[index].date;
+    this.post.time=this.events[index].time;
+    this.index=index;
   }
 }
