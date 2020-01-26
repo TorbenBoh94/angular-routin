276d83afaa420756af297043ecae33dd9e6c599f
bugyman <torben_barmstedt@musicmeister.de>
2020-01-26 13:35:10 +0100

    1 file modified

--- src/app/pages/events/event.service.ts
+++ src/app/pages/events/event.service.ts
@@ -6,8 +6,13 @@ export class EventService {
  
  edit=false;
  index:number;
- events=[ new Events("Hochschulfest","Gemütliches Beisammensitzen der Studenten und Professoren","https://me2be.de/wp-content/uploads/2018/06/CAMPUS_Sommerfest_HSFL_Dorbandt_1.jpg","2020-08-15","15:00"),
- new Events("Semesterbeginn","Das Semester beginnt und das Lernen kann wieder anfangen","https://hs-flensburg.de/sites/default/files/styles/full_width/public/mood/termine_Web.jpg?itok=w1xB9zKc","2020-03-01",""),
+ //Für die Links der Bilder bzw. der Bilder sollte eine Einheitliche Größe genommen werden, was hier im Beispiel erstmal nicht der Fall ist
+events=[ new Events("Hochschulfest","Gemütliches Beisammensitzen der Studenten und Professoren","https://me2be.de/wp-content/uploads/2018/06/CAMPUS_Sommerfest_HSFL_Dorbandt_1.jpg","2020-08-15","15:00"),
+  new Events("Semesterbeginn","Das Semester beginnt und das Lernen kann wieder anfangen","https://hs-flensburg.de/sites/default/files/styles/full_width/public/mood/termine_Web.jpg?itok=w1xB9zKc","2020-03-01",""),
+  new Events("Virtual Play Day","Das Semester beginnt und das Lernen kann wieder anfangen","https://hs-flensburg.de/sites/default/files/styles/frontpage_s/public/aktuelles/2020-01/bild.png?h=2765ece5&itok=W5ohgyzH","2020-02-05",""),
+  new Events("Workshop Lebensqualität","Um pflegende Angehörige in ihrer täglichen Arbeit zu unterstützen, arbeiten derzeit deutsche und dänische Partner im Interreg-Projekt Demantec zusammen","","2020-02-01",""),
+  new Events("Global Game Jam","Auch in diesem Jahr sind wir wieder dabei, wenn es ein Wochenende lang heißt: Spiele vor Schlaf. 48 Stunden lang werden Spiele erdacht, entwickelt, designt, programmiert, getestet und am Ende präsentiert. ","","2020-01-31",""),
+  new Events("Konventssitzung","Bitte beachten Sie: Dieser Termin findet nur bei Bedarf statt.","","2020-02-12",""),
  ];
 
  post={
